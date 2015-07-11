925e7d4bd8617efc3ed6a4d4ac7186b54864288d
Andrew Lingenfelter <luridsorcerer@gmail.com>
2015-07-11 03:42:42 -0400

    commented out non-working php code

--- index.php
+++ index.php
@@ -10,16 +10,16 @@
 
 // Provides access to app specific values such as your app id and app secret.
 // Defined in 'AppInfo.php'
-require_once('AppInfo.php');
+//require_once('AppInfo.php');
 
 // Enforce https on production
-if (substr(AppInfo::getUrl(), 0, 8) != 'https://' && $_SERVER['REMOTE_ADDR'] != '127.0.0.1') {
-  header('Location: https://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
-  exit();
+//if (substr(AppInfo::getUrl(), 0, 8) != 'https://' && $_SERVER['REMOTE_ADDR'] != '127.0.0.1') {
+//  header('Location: https://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+//  exit();
 }
 
 // This provides access to helper functions defined in 'utils.php'
-require_once('utils.php');
+//require_once('utils.php');
 
 
 /*****************************************************************************
@@ -31,52 +31,52 @@ require_once('utils.php');
  *
  ****************************************************************************/
 
-require_once('sdk/src/facebook.php');
+//require_once('sdk/src/facebook.php');
 
-$facebook = new Facebook(array(
-  'appId'  => AppInfo::appID(),
-  'secret' => AppInfo::appSecret(),
-  'sharedSession' => true,
-  'trustForwarded' => true,
-));
+//$facebook = new Facebook(array(
+//  'appId'  => AppInfo::appID(),
+  // 'secret' => AppInfo::appSecret(),
+  // 'sharedSession' => true,
+  // 'trustForwarded' => true,
+// ));
 
-$user_id = $facebook->getUser();
-if ($user_id) {
-  try {
-    // Fetch the viewer's basic information
-    $basic = $facebook->api('/me');
-  } catch (FacebookApiException $e) {
-    // If the call fails we check if we still have a user. The user will be
-    // cleared if the error is because of an invalid accesstoken
-    if (!$facebook->getUser()) {
-      header('Location: '. AppInfo::getUrl($_SERVER['REQUEST_URI']));
-      exit();
-    }
-  }
+// $user_id = $facebook->getUser();
+// if ($user_id) {
+  // try {
+    Fetch the viewer's basic information
+    // $basic = $facebook->api('/me');
+  // } catch (FacebookApiException $e) {
+    If the call fails we check if we still have a user. The user will be
+    cleared if the error is because of an invalid accesstoken
+    // if (!$facebook->getUser()) {
+      // header('Location: '. AppInfo::getUrl($_SERVER['REQUEST_URI']));
+      // exit();
+    // }
+  // }
 
   // This fetches some things that you like . 'limit=*" only returns * values.
   // To see the format of the data you are retrieving, use the "Graph API
   // Explorer" which is at https://developers.facebook.com/tools/explorer/
-  $likes = idx($facebook->api('/me/likes?limit=4'), 'data', array());
+  // $likes = idx($facebook->api('/me/likes?limit=4'), 'data', array());
 
   // This fetches 4 of your friends.
-  $friends = idx($facebook->api('/me/friends?limit=4'), 'data', array());
+  // $friends = idx($facebook->api('/me/friends?limit=4'), 'data', array());
 
   // And this returns 16 of your photos.
-  $photos = idx($facebook->api('/me/photos?limit=16'), 'data', array());
+  // $photos = idx($facebook->api('/me/photos?limit=16'), 'data', array());
 
   // Here is an example of a FQL call that fetches all of your friends that are
   // using this app
-  $app_using_friends = $facebook->api(array(
-    'method' => 'fql.query',
-    'query' => 'SELECT uid, name FROM user WHERE uid IN(SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1'
-  ));
-}
+  // $app_using_friends = $facebook->api(array(
+    // 'method' => 'fql.query',
+    // 'query' => 'SELECT uid, name FROM user WHERE uid IN(SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1'
+  // ));
+// }
 
 // Fetch the basic info of the app that they are using
-$app_info = $facebook->api('/'. AppInfo::appID());
+// $app_info = $facebook->api('/'. AppInfo::appID());
 
-$app_name = idx($app_info, 'name', '');
+// $app_name = idx($app_info, 'name', '');
 
 ?>
 <html>
