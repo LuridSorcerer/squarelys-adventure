4d4312ddc9d184c8edce9b900338ff1593a451cd
Andrew Lingenfelter <luridsorcerer@gmail.com>
2015-07-11 23:01:44 -0400

    added Facebook Graph API

--- index.php
+++ index.php
@@ -26,6 +26,25 @@ head, body { margin: 0px;}
 
 <body>
 
+    <script>
+
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '320216424781470',
+          xfbml      : true,
+          version    : 'v2.4'
+        });
+      };
+
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+    </script>
+
 <!--
 <h3 align="center">Squarely's Adventure</H3>
 -->
