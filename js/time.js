9eeca234315aa5f41df54ca5a65ecd09ff10326e
Andrew Lingenfelter <luridsorcerer@gmail.com>
2025-06-19 13:47:10 -0400

    Capped delta time value

--- js/time.js
+++ js/time.js
@@ -1,6 +1,7 @@
 export const Time = {
 	now: 0,
 	delta: 0,
+	DELTA_CAP: 50,
 	init: function() {
 		this.now = Date.now();
 		this.delta = 0;
@@ -8,6 +9,7 @@ export const Time = {
 	update: function() {
 		let current = Date.now();
 		this.delta = current - this.now;
+		if(this.delta > this.DELTA_CAP) { this.delta = this.DELTA_CAP; }
 		this.now = current;
 	}
 };
